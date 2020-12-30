ba9eda88a01673788898ad15e8d8b631e9c95fea
Markus Pauer <markus@diepauers.de>
2020-12-30 15:46:10 +0100

    Using Camunda with a separate H2-DB

--- src/main/java/de/markuspauer/quarkus/rest/ProcessEngineEndpoint.java
+++ src/main/java/de/markuspauer/quarkus/rest/ProcessEngineEndpoint.java
@@ -4,8 +4,13 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.camunda.bpm.engine.RepositoryService;
 
 import de.markuspauer.quarkus.service.ProcessEngineService;
+import io.quarkus.vertx.http.runtime.devmode.Json;
 
 @Path("engine")
 @RequestScoped
@@ -13,10 +18,19 @@ public class ProcessEngineEndpoint {
 
     @Inject
     ProcessEngineService processEngineService;
+
+    @Inject
+    RepositoryService repositoryService;
     
     @GET
-    public String getEngineName() {
-        return processEngineService.getName();
+    @Path("info")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getEngineInfo() {
+        return Json
+        .object()
+        .put("engine", processEngineService.getName())
+        .put("processes", repositoryService.createProcessDefinitionQuery().count())
+        .build();
     }
 
 }
--- src/main/java/de/markuspauer/quarkus/service/ProcessEngineService.java
+++ src/main/java/de/markuspauer/quarkus/service/ProcessEngineService.java
@@ -4,22 +4,57 @@ import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.RepositoryService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+import io.quarkus.runtime.StartupEvent;
 
 @ApplicationScoped
 public class ProcessEngineService implements Serializable {
 
+    private static final Logger LOG = Logger.getLogger(ProcessEngineService.class);
+
     private ProcessEngine engine;
 
+    @ConfigProperty(name = "camunda.engine.name")
+    private String processEngineName;
+    
+    @ConfigProperty(name = "camunda.db.url")
+    private String camundaDbUrl;
+
+    @ConfigProperty(name = "camunda.db.user")
+    private String camundaDbUser;
+
+    @ConfigProperty(name = "camunda.db.pass")
+    private String camundaDbPass;
+
     @PostConstruct
-    void init() {
-        engine = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration().buildProcessEngine();
+    void init(@Observes StartupEvent event) {
+        engine = ProcessEngineConfiguration
+        .createStandaloneProcessEngineConfiguration()
+        .setProcessEngineName(processEngineName)
+        .setJdbcDriver("org.h2.Driver")
+        .setJdbcUrl(camundaDbUrl)
+        .setJdbcUsername(camundaDbUser)
+        .setJdbcPassword(camundaDbPass)
+        .setDatabaseSchemaUpdate("true")
+        .buildProcessEngine();
     }
 
     public String getName() {
         return engine.getName();
+    }
+
+    @Produces
+    RepositoryService getRepositoryService() {
+        return engine.getRepositoryService();
     }
     
 }
