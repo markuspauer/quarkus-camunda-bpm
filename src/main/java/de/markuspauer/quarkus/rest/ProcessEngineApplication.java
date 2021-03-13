e71aed310c3f3fdc497933354b923439ab0b99a0
Markus Pauer <markus@diepauers.de>
2021-03-13 12:52:26 +0100

    Switching Endpoints to api context path and static resources to root context

--- src/main/java/de/markuspauer/quarkus/rest/ProcessEngineApplication.java
+++ src/main/java/de/markuspauer/quarkus/rest/ProcessEngineApplication.java
@@ -5,7 +5,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @Dependent
-@ApplicationPath("/")
+@ApplicationPath("/api")
 public class ProcessEngineApplication extends Application {
     
 }
